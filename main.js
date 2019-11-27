33dfe0e7199306f4b474cb43c86382824bee7d50
李松霖 <lisonglin@corp.netease.com>
2019-11-28 00:10:00 +0800

    fix: 1

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var playInfo = document.getElementById('info');
 var isPlay = false;
 
 audio.addEventListener('waiting', () =>{ playInfo.innerText = 'loading'});
-audio.addEventListener('playing', () =>{ playInfo.innerText = ''});
+audio.addEventListener('canplaythrough', () =>{ playInfo.innerText = ''});
 var button = document.createElement('button');
 button.style = "background: #333; width: 100px; height: 50px; left: 50%; top: 50%; color: #fff;";
 button.innerText = '播放';
