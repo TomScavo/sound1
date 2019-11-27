21643121e9372b53343f2df352c26f91466bf826
李松霖 <lisonglin@corp.netease.com>
2019-11-27 22:47:01 +0800

    feat: first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+var audio = new Audio('https://cdn.musicbed.com/video/authenticated/s--I0LUjcCx--/af_48000,br_96k,du_30/ac_vorbis/v1554920665/production/songs/24242.ogg');
+
+var playInfo = document.getElementById('info');
+
+var isPlay = false;
+
+audio.addEventListener('waiting', () =>{ playInfo.innerText = 'loading'});
+audio.addEventListener('playing', () =>{ playInfo.innerText = ''});
+var button = document.createElement('div');
+button.style = "background: #333; width: 100px; height: 50px; left: 50%; top: 50%; color: #fff;";
+button.innerText = '播放';
+button.addEventListener('click', () => {
+        isPlay ? audio.pause() : audio.play()
+        button.innerText = isPlay ? '暂停' : '播放';
+        isPlay = !isPlay
+    }
+)
+
+var body = document.getElementsByTagName('body');
+
+
+body[0].append(button);
